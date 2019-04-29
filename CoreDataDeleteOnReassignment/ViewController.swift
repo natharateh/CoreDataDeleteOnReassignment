51a09b2da7749e40e51feac24b29dbe4a9e04691
Natalia Harateh <nambatee@users.noreply.github.com>
2019-04-29 16:40:23 -0400

    Initial Commit

---
+++ CoreDataDeleteOnReassignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDeleteOnReassignment
+//
+//  Created by Natalia Harateh on 4/29/19.
+//  Copyright © 2019 Natalia Harateh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
